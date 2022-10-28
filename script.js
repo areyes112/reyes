473a694002d3c1cc1d9648142b63a0e6936384b5
areyes2004 <115803971+areyes112@users.noreply.github.com>
2022-10-28 14:25:19 +0000

    connected replit to github.

---
+++ script.js
@@ -0,0 +1,12 @@
+// examples function call to display a dialog box
+// Lets comment out the code below, which is top-level code 
+// thats automatically runs when the script is loaded
+//alert("welcome, this is a javascript popup alert");
+
+function headingAlert(whichone) {
+  alert("you clicked a heading: " + whichone);
+}
+
+function goodbyeMessage() {
+  alert('are you sure you want to leave?');
+}
