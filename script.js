d9542cd1ca808c13633c1c8d2eba25c62f864355
areyes2004 <115803971+areyes112@users.noreply.github.com>
2022-11-04 14:24:28 +0000

    worked on local storage

--- script.js
+++ script.js
@@ -3,10 +3,50 @@
 // thats automatically runs when the script is loaded
 //alert("welcome, this is a javascript popup alert");
 
-function headingAlert(whichone) {
-  alert("you clicked a heading: " + whichone);
+function submitForm(name, hometown) {
+  //search the DOM for a specific tag with the id "guestlist"
+  var guestlist = document.getElementById("guestlist");
+  var n = document.getElementById("name");
+  var h = document.getElementById("hometown");
+  var currentguestlist = localStorage.getItem("guestlist");
+  localStorage.setItem("guestlist", currentguestlist + "<li>" + name + ". " + hometown + "</li>");
+  guestlist.innerHTML += "<li>" + name + ". " + hometown + "</li>"; 
+  n.value = "";
+  h.value = "";
+  n.focus();
+  return false; 
+}
+function clearStorageGB() {
+  var guestlist = document.getElementById("guestlist");
+  guestlist.innerHTML = "";
+  localStorage.setItem("guestlist","");
 }
 
-function goodbyeMessage() {
-  alert('are you sure you want to leave?');
+function checkStorageGB(){
+  var guestlist = document.getElementById("guestlist");
+  var currentguestlist = localStorage.setItem("guestlist");
+  guestlist.innerHTML = currentguestlist;
+}
+
+function clearStorage() {
+  var answerlist = document.getElementById("answerlist");
+  answerlist.innerHTML = "";
+  localStorage.setItem("answerlist","");
+}
+
+function checkStorage(){
+  var answerlist = document.getElementById("answerlist");
+  var currentanswers = localStorage.setItem("answerlist");
+  answerlist.innerHTML = currentanswers;
+}
+
+function submitAnswer(answer) {
+  //search the DOM for a specific tag with the id "guestlist"
+  var answerlist = document.getElementById("answerlist");
+  var currentanswers = localStorage.getItem("answerlist");
+  localStorage.setItem("answerlist", currentanswers + "<br/>" + answer.value);
+  answerlist.innerHTML += answer.value + "<br />"; 
+  answer.value=""
+  answer.focus();
+  return false; 
 }
